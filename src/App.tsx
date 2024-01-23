1169c2b3a58b78780631dde4c9b58198401098d3
mateusz <Mateusz.S-ki@proton.me>
2024-01-23 21:55:37 +0100

    files

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  const urlgifa =
+    "https://cdn.dribbble.com/users/1437488/screenshots/4338156/media/7554f668510449916f9235907285d415.gif";
+  return (
+    <div className="flexwrapper">
+      <div className="main">
+        <h1>
+          In construction.... <br />
+          (expect around two houndred years before i deploy this)
+          <br />
+          <br />
+          <br />
+          <br />
+          <img width={400} height={300} src={urlgifa} />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br /> PS. I hate React
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
